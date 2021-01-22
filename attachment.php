ce6fb3c39f68cf6fe43899a07f3c819d28b0130e
Kirill Shcherbina <rabbichaim770@gmail.com>
2021-01-21 22:13:11 -0600

    image.php

---
+++ attachment.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+  <div id="primary" class="content-area">
+
+    <main id="main" class="site-main" role="main">
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+              
+      <article id="post-<?php the_ID(); ?>"  <?php post_class(); ?>>
+
+<header class="entry-header">
+
+  <?php echo  get_post_format($post->ID); ?>
+
+  <?php the_title( '<h1>', '</h1>' ); ?>
+
+  <div class="byline">
+    <?php esc_html_e( 'Author:' ); ?> <?php the_author(); ?>
+  </div>
+
+</header>
+
+<div class="entry-content">
+
+  <p><a href="<?php echo $post->guid; ?>">Download</a></p>
+
+  <?php the_content(); ?>
+
+</div>
+
+<?php if( comments_open() ) : ?>
+
+  <?php comments_template(); ?>
+
+<?php endif; ?>
+
+
+
+</article>
+
+
+
+
+
+      <?php endwhile; else : ?>
+
+        <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+      <?php endif; ?>
+
+      <p>Template: attachment.php</p>
+
+    </main>
+
+  </div>
+
+  <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
