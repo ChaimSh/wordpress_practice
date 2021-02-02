13e9a251540ba9c88866fb91401f76ce8c4d6c6b
Kirill Shcherbina <rabbichaim770@gmail.com>
2021-02-01 20:43:04 -0600

    single-port

--- single-portfolio.php
+++ single-portfolio.php
@@ -5,14 +5,30 @@
     <main id="main" class="site-main" role="main">
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    
+                    <article id="post-<?php the_ID(); ?>"  <?php post_class(); ?>>
 
-        <?php get_template_part( 'template-parts/content' ); ?>
+                        <header class="entry-header">
 
-      <?php endwhile; else : ?>
+                                <?php the_title( '<h1>', '</h1>' ); ?>
 
-        <?php get_template_part( 'template-parts/content', 'none' ); ?>
+                        </header>
 
-      <?php endif; ?>
+                        <div class="entry-content">
+
+                        
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail( 'full' ); ?>
+                            </a>
+
+
+                            <?php the_content(); ?>
+
+                        </div>
+
+                    </article>
+
+      <?php endwhile; endif;?>
 
       <p>Template: single-portfolio.php</p>
 
