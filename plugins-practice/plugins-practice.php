2314de990d5a7a2d000b3c72cbb0749ab70131a7
Kirill Shcherbina <rabbichaim770@gmail.com>
2021-03-10 20:21:16 -0600

    plugin setting page

--- plugins-practice/plugins-practice.php
+++ plugins-practice/plugins-practice.php
@@ -26,5 +26,15 @@ add_action( 'admin_menu', 'wpplugin_settings_page' );
 
 function wpplugin_settings_page_markup()
 {
-    
+    if( !current_user_can('manage_options') ) {
+        return;
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php esc_html_e( get_admin_page_title() ); ?></h1>
+        <p><?php esc_html_e( 'Some content.' ); ?></p>
+        </div>
+      <?php  
 }
+
+?>
